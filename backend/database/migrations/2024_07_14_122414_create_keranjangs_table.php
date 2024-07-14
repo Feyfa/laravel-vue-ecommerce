4633c15953da96d1e0986cc578053df9e5ab8b4b
Feyfa <fisikamodern00@gmail.com>
2024-07-14 19:45:02 +0700

    - membuat model, migrasi, seeder

--- backend/database/migrations/2024_07_14_122414_create_keranjangs_table.php
+++ backend/database/migrations/2024_07_14_122414_create_keranjangs_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('keranjangs', function (Blueprint $table) {
             $table->id();
-            $table->string('img')->nullable();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('product_id')->constrained('products');
+            $table->foreignId('user_id_buyer')->constrained('users');
+            $table->boolean('checked')->default(false);
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('keranjangs');
     }
 };
